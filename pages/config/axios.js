c96ce8075e89b1ed135825a5aac2c2340e72031b
kevin2sant <kevin770540@outlook.com>
2022-03-17 10:28:47 -0300

    update

--- pages/config/axios.js
+++ pages/config/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const clientAxios = axios.create({
-    baseURL: 'http://localhost:3001/',
+    baseURL: 'https://pnxwxr066j.execute-api.us-east-1.amazonaws.com/',
     timeout: 10000,
     // headers: {'X-Custom-Header': 'foobar'}
 })
