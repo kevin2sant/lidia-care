2c2efb24c3c8027f3447ae8c3f7f82213967a430
kevin2sant <kevin770540@outlook.com>
2022-03-20 03:43:59 -0300

    updates

--- pages/layouts/body.js
+++ pages/layouts/body.js
@@ -1,28 +1,14 @@
 import React from 'react'
 import Sidebar from './sidebar'
 import Footer from './footer'
-import { useSelector } from 'react-redux'
-import Home from '../'
-import { useRouter } from 'next/router'
 
 const Body = () => {
-
-
-    const router = useRouter();
-  
-    const auth = useSelector( state => state.login.auth)
-
-    if(auth){
-        return (
-            <>
-                <Sidebar/>
-                <Footer/>
-            </>
-        )
-    }else{
-        router.replace('/')
-        return null
-    }
+    return (
+        <>
+            <Sidebar/>
+            <Footer/>
+        </>
+    )
 }
 
 export default Body;
