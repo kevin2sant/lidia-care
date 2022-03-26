3679ad49a0c0c0991ce4b8c3d9c69767f193465d
kevin2sant <kevin770540@outlook.com>
2022-03-26 04:57:45 -0300

    updates

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
+import Document, { Html, Head, Main, NextScript  } from "next/document";
+import Script from 'next/script'
 
 class MyDocument extends Document {
   render() {
@@ -10,7 +11,8 @@ class MyDocument extends Document {
           <meta name="theme-color" content="#fff" />
 
           <link rel="apple-touch-icon" href="%PUBLIC_URL%/logo192.png" />
-          
+          <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"></link>
+
           {/* <!-- Metas para el iphone --> */}
           <meta name="theme-color" content="#9BDFE6"/>
           <meta name="mobile-web-app-capable" content="yes" />
@@ -23,6 +25,7 @@ class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
+          
         </body>
       </Html>
     );
