cd95c017a97fa6e0669d9ad83d5e5c7199e6566a
kevin2sant <kevin770540@outlook.com>
2022-03-27 22:19:37 -0300

    updates

--- pages/user/register.js
+++ pages/user/register.js
@@ -2,7 +2,6 @@ import React, {useState, useEffect} from 'react'
 
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
-import Paper from '@mui/material/Paper';
 import { styled } from '@mui/material/styles';
 import Body from '../../components/layouts/body';
 import BasicTable from '../../components/user/tableUser';
@@ -10,13 +9,25 @@ import styles from '../../styles/User.module.css';
 import clientAxios from '../../config/axios';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
+import InputLabel from '@mui/material/InputLabel';
 
+import FormControl from '@mui/material/FormControl';
 import CircularProgress from '@mui/material/CircularProgress';
 import MenuItem from '@mui/material/MenuItem';
 import Select from '@mui/material/Select';
 import OutlinedInput from '@mui/material/OutlinedInput';
 import MuiAlert from '@mui/material/Alert';
 import Snackbar from '@mui/material/Snackbar';
+
+
+// tabla
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
 const Alert = React.forwardRef(function Alert(props, ref) {
     return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
@@ -30,21 +41,22 @@ const Item = styled(Paper)(({ theme }) => ({
 
 const RegisterUser = () => {
     const [dataForm, setDataForm] = useState({
-        company : false,
-        plan : false,
-        users : false
+        company : '',
+        plan : '',
+        users : ''
     })
     const [error, setError] = useState({
-        type : false,
-        message : false
+        type : '',
+        message : ''
     })
 
     const [userAdd, setUserAdd] = useState(false)
 
     const [open, setOpen] = useState(false);
     const [file, setFile] = useState();
-    const [plan, setPlan] = useState(false)
-    const [company, setCompany] = useState(false)
+    const [plan, setPlan] = useState('')
+    const [company, setCompany] = useState('')
+    const [loading, setLoading] = useState(false)
     
     const handleClick = () => {
         setOpen(true);
@@ -58,11 +70,10 @@ const RegisterUser = () => {
         setOpen(false);
     };
     
-    const setForm = (e) => {
-        console.log(e)
+    const setForm = (event) => {
         setDataForm({
                 ...dataForm,
-                [e.target.name] : e.target.value
+                [event.target.name] : event.target.value
         })
     }
     // al cargar la pagina cargara los datos de la compañias y planes 
@@ -109,14 +120,14 @@ const RegisterUser = () => {
         e.preventDefault();
         setUserAdd(false)
         setError({
-            type : false,
-            message : false
+            type : '',
+            message : ''
         })
         
         if(!dataForm.company){
             setError({
                 type : 'error',
-                message : 'Debe seleccionar la compañia'
+                message : 'DEBE SELECCIONAR UNA COMPAÑIA'
             })
             handleClick()
             return false
@@ -125,7 +136,7 @@ const RegisterUser = () => {
         if(!dataForm.plan){
             setError({
                 type : 'error',
-                message : 'Debe seleccionar el plan'
+                message : 'DEBE SELECCIONAR EL PLAN'
             })
             
             handleClick()
@@ -136,28 +147,31 @@ const RegisterUser = () => {
             
             setError({
                 type : 'error',
-                message : 'Debe seleccionar el archivo'
+                message : 'DEBE SELECCIONAR EL ARCHIVO'
             })
 
             handleClick()
             return false
         }
 
+        setLoading(true)
         clientAxios.post('care/user/usersRegister', dataForm)
         .then(res => {
             setUserAdd(res.data.data)
             setError({
                 type : 'success',
-                message : 'Los datos se registraron correctamente'
+                message : 'LOS DATOS SE REGISTRARON CORRECTAMENTE'
             })
             handleClick()
+            setLoading(false)
         })
         .catch(err => {
             console.log(err)
             setError({
                 type : 'error',
-                message : 'Hubo un error al ingresar los datos'
+                message : 'HUBO UN ERROR AL INGRESAR LOS DATOS'
             })
+            setLoading(false)
         })
     };
 
@@ -167,55 +181,56 @@ const RegisterUser = () => {
             <Box sx={{ flexGrow: 1, marginTop :'70px'}}>
                 <Grid container spacing={2}>
                     
-                    <Grid item xs={12} sm={12} md={4} xl={4}>
+                    <Grid item xs={12} sm={12} md={6} xl={6}>
                         <Item>
-                        Seleccione compañia<br/><br/>
                         {company ? 
+                            <FormControl fullWidth>
+                            <InputLabel id="demo-simple-select-label">Compañia</InputLabel>
                             <Select
-                                labelId="Compañia"
                                 id="company"
                                 name="company"
                                 label="Compañia"
                                 className={styles.select}
                                 input={<OutlinedInput label="Compañia" />}
-                                onChange={(e) => setForm(e)}
+                                onChange={setForm}
+                                value={dataForm.company}
                             >
                                 {company.map((value,item) => (
                                     <MenuItem key={value.i_idcompany} value={value.i_idcompany}>{value.v_company}</MenuItem>
                                 ))}
                                 
                             </Select>
+                            </FormControl>
                         :
                         <center><CircularProgress /></center>
                         }
                         </Item>
-                    </Grid>
-
-                    <Grid item xs={12} sm={12} md={4} xl={4}>
+                        <br/>
                         <Item>
-                        Seleccione plan<br/><br/>
                         {plan ? 
+                            <FormControl fullWidth>
+                            <InputLabel id="demo-simple-select-label">Plan</InputLabel>
                             <Select
                                 labelId="Plan"
                                 id="plan"
                                 name="plan"
                                 label="Plan"
                                 className={styles.select}
-                                input={<OutlinedInput label="Plan" />}
                                 onChange={setForm}
+                                value={dataForm.plan}
                             >
                                 {plan.map((value,item) => (
                                     <MenuItem key={value.i_idplan} value={value.i_idplan}>{value.v_plan}</MenuItem>
                                 ))}
                                 
                             </Select>
+                            </FormControl>
                         :
                         <center><CircularProgress /></center>
                         }
                         </Item>
                     </Grid>
-
-                    <Grid item xs={12} sm={12} md={4} xl={4}>
+                    <Grid item xs={12} sm={12} md={6} xl={6}>
                         <Item>
                         Cargar Archivo con usuarios<br/><br/>
                         <form>
@@ -227,35 +242,46 @@ const RegisterUser = () => {
                                 type={"file"}
                                 onChange={handleOnChange}
                             /><br/><br/>
-
-                            <Button class="btn btn-block btn-primary" onClick={(e) => { handleOnSubmit(e) }} variant="contained">Cargar Usuarios</Button>
+                            {loading ? 
+                                <center><CircularProgress /></center>
+                            : 
+                                <Button className="btn btn-block btn-primary" onClick={(e) => { handleOnSubmit(e) }} variant="contained">Cargar Usuarios</Button>
+                            }
                         </form>
                         
                         </Item>
                     </Grid>
+
+                    
                     {userAdd && (
                         <Grid item xs={12} sm={12} md={12} xl={12}>
                             <Item> 
-                                <table className="table table-bordered table-hover">
-                                    <thead>
-                                        <th>Usuario</th>
-                                        <th>Estado</th>
-                                    </thead>  
-                                    <tbody>
-                                    {userAdd.map((value,item) => (
-                                        <tr key={item}>
-                                            <td key={item}>{value.v_identification}</td>
-                                            <td key={item}>{value.status}</td>
-                                        </tr>
-                                    ))}
-                                    </tbody>    
-                                </table>                 
+                            <TableContainer component={Paper}>
+                            <Table sx={{ minWidth: 650 }} aria-label="caption table">
+                                <caption>Usuarios importados desde el archivo.</caption>
+                                <TableHead>
+                                <TableRow>
+                                    <TableCell align="center">Usuario</TableCell>
+                                    <TableCell align="center">Estado</TableCell>
+                                </TableRow>
+                                </TableHead>
+                                <TableBody>
+                                {userAdd.map((value,item) => (
+                                    <TableRow key={item}>
+                                    <TableCell align="center">{value.v_identification}</TableCell>
+                                    <TableCell align="center">{value.status}</TableCell>
+                                    </TableRow>
+                                ))}
+                                </TableBody>
+                            </Table>
+                            </TableContainer>          
                             </Item>
                         </Grid>
                     )}
                 </Grid>
             </Box>
         </Body>
+
         <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
             <Alert onClose={handleClose} severity={error.type} sx={{ width: '100%' }}>
                 {error.message}
